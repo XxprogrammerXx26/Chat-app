b7b15924034a4d19141049c937dbaf340508ce45
Trabajo_Android <lv9334889@gmail.com>
2024-11-26 15:16:51 -0300

    Delete this codes

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -2,10 +2,15 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import socketio from 'socket.io-client';
+
+
+
+
+const socket = socketio('http://localhost:3000');
 
 const app = createApp(App)
+app.config.globalProperties.$socket = socket;
 
-app.use(router)
 
 app.mount('#app')
